793ec92137eb2c11283f5eb5d3ee6e6151f619c5
torresriquelme <torres.riquelme@gmail.com>
2016-07-17 07:22:24 -0400

    Utilizando FormatJS para internacionalizar las fechas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 var page = require('page');
-var moment = require('moment');
 
-require('moment/locale/es');
-moment.locale('es');
 
 require('./homepage');
 require('./signup');
--- src/picture-card/index.js
+++ src/picture-card/index.js
@@ -1,5 +1,17 @@
 var yo = require('yo-yo');
 var moment = require('moment');
+if(!window.Intl){
+  window.Intl = require('intl');
+  require('intl/locale-data/jsonp/en-US.js');
+  require('intl/locale-data/jsonp/es.js');
+  require('intl/locale-data/jsonp/fr.js');
+}
+var IntlRelativeFormat = window.IntlRelativeFormat = require('intl-relativeformat');
+require('intl-relativeformat/dist/locale-data/en.js');
+require('intl-relativeformat/dist/locale-data/fr.js');
+require('intl-relativeformat/dist/locale-data/es.js');
+
+var rf = new IntlRelativeFormat('fr');
 
 module.exports = function pictureCard(pic) {
 
@@ -15,7 +27,7 @@ module.exports = function pictureCard(pic) {
           <img src="${picture.user.avatar}" class="avatar"/>
           <span class="username">${picture.user.username}</span>
         </a>
-        <small class="right time">${moment(picture.createdAt).fromNow()}</small>
+        <small class="right time">${rf.format(picture.createdAt)}</small>
         <p>
           <a class="left" href="#" onclick=${like.bind(null, true)}><i class="fa fa-heart-o" aria-hidden="true"></i></a>
           <a class="left" href="#" onclick=${like.bind(null, false)}><i class="fa fa-heart" aria-hidden="true"></i></a>
