5956c3dc952d950f75659ea2466bf387006fdf4b
torresriquelme <torres.riquelme@gmail.com>
2016-07-21 07:06:40 -0400

    Subiendo la foto a nuestro servidor web

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 var express = require('express');
+var multer = require('multer');
+var ext = require('file-extension');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, +Date.now() + '.' + ext(file.originalname))
+  }
+});
+
+//El nombre que esta en el argumento de single debe ser el name que esta en la etiqueta HTML tipo input con el cual se hace el post
+var upload = multer({ storage: storage }).single('picture');
+
 var app = express();
 
 app.set('view engine', 'pug');
@@ -43,6 +58,15 @@ app.get('/api/pictures', function(req, res){
 	setTimeout(function () {
 		res.send(pictures);
 	}, 2000)
+});
+
+app.post('/api/pictures', function(req, res) {
+	upload(req, res, function(err) {
+		if(err){
+			return res.send(500, "Error uploading file");
+		}
+		res.send('File uploaded successfully');
+	})
 })
 
 app.listen(3000, function(err){
--- src/homepage/template.js
+++ src/homepage/template.js
@@ -2,12 +2,13 @@ var yo = require('yo-yo');
 var layout = require('../layout');
 var picture = require('../picture-card');
 var translate = require('../translate').message;	
+var request = require('superagent');
 
 module.exports = function (pictures) {
 	var el = yo`<div class="container timeline">
 			<div class="row">
 				<div class="col s12 m10 offset-m1 l8 offset-l2 center-align">
-					<form enctype="multipart/form-data" class="form-upload" id="formUpload">
+					<form enctype="multipart/form-data" class="form-upload" id="formUpload" onsubmit=${onsubmit}>
 						<div id="fileName" class="fileUpload btn btn-flat cyan">
 							<span><i class="fa fa-camera" aria-hidden="true"></i>${translate('upload-picture')}</span>
 							<input name="picture" id="file" type="file" class="upload" onchange=${onchange}/>
@@ -40,6 +41,21 @@ module.exports = function (pictures) {
 
 		function onchange() {
 			toggleButtons();
+		}
+
+		function onsubmit(event) {
+			//Como en el formulario no se especifica que tipo de submit se usa (GET o POST) la funcion prevenDefault nos
+			//permite codificarla desde el metodo que llamamos en el formulario y no se ejecutara el request innecesario
+			event.preventDefault();
+			//El objeto FormData es un objeto que se dispara por el mismo formulario. this hace referencia al formulario
+			var data = new FormData(this);
+			//Se usa superagent para comunicarse con el servidor, ahora con el metodo post y a la direccion '/api/pictures'
+			request
+				.post('/api/pictures')
+				.send(data)
+				.end(function(err, res) {
+					console.log(arguments);
+				})
 		}
 
 	return layout(el);	
