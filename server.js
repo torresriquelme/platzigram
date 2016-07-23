2f555ee6922b1f6277282ac0ae0b122986738f62
torresriquelme <torres.riquelme@gmail.com>
2016-07-22 21:13:59 -0400

    Desafio 1 Hacer la pagina de un usuario

--- server.js
+++ server.js
@@ -55,9 +55,9 @@ app.get('/api/pictures', function(req, res){
 			createdAt: new Date().setDate(new Date().getDate() - 10)
 		}
 	];
-	setTimeout(function () {
-		res.send(pictures);
-	}, 2000)
+	
+	res.send(pictures);
+	
 });
 
 app.post('/api/pictures', function(req, res) {
@@ -67,6 +67,45 @@ app.post('/api/pictures', function(req, res) {
 		}
 		res.send('File uploaded successfully');
 	})
+})
+
+app.get('/api/user/:username', function(req, res){
+	const user = {
+		username: 'torresriquelme',
+		avatar: 'https://pbs.twimg.com/profile_images/492480625635770368/g8PQEzBI.jpeg',
+		pictures: [
+			{
+				id: 1,
+				src: 'http://www.g3galeon.com/wp-content/uploads/2015/12/Madrid1.jpg',
+				likes: 3
+			},
+			{
+				id: 2,
+				src: 'https://d16teuje7e44sv.cloudfront.net/spa/cities/spain/madrid-medium.jpg',
+				likes: 5
+			},
+			{
+				id: 3,
+				src: 'http://www.enforex.com/images/fichas/madrid/ciudad-madrid-5.jpg',
+				likes: 2
+			},
+			{
+				id: 4,
+				src: 'http://www.nh-hoteles.es/multimedia/images/madrid-140940024_600x458-tcm43-132903-32.jpg',
+				likes: 10
+			},
+			{
+				id: 5,
+				src: 'https://cache-graphicslib.viator.com/graphicslib/thumbs360x240/22869/SITours/madrid-y-monasterio-de-el-escorial-en-un-tour-tur-stico-de-un-d-a-de-in-madrid-259570.jpg',
+				likes: 10
+			}
+		]
+	}
+	res.send(user);
+})
+
+app.get('/:username', function (req, res){
+	res.render('index', { title: `Platzigram - ${req.params.username}`})
 })
 
 app.listen(3000, function(err){
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ var page = require('page');
 require('./homepage');
 require('./signup');
 require('./signin');
+require('./user-page');
 require('./footer');
 
 //Con este comando se pone a correr PageJS. Es requerido.....
